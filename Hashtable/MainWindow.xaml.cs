71f7fc04e96c1126c20461037b0d672c98f7484a
jomjamdkk <jomjamdkk@gmail.com>
2022-02-05 21:20:08 +0700

    DoHomework

--- Hashtable/MainWindow.xaml.cs
+++ Hashtable/MainWindow.xaml.cs
@@ -1,28 +1,81 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace Hashtable
+namespace hashTable_
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        ICollection collection;
+        Hashtable hashtable;
         public MainWindow()
         {
             InitializeComponent();
+            hashtable = new Hashtable();
         }
+        private void AddBtn_Click(object sender, RoutedEventArgs e)
+        {
+            hashtable.Add(Key_TxtBox.Text.ToString(), Value_TxtBox.Text.ToString()) ;
+        }
+        private void RemoveBtn_Click(object sender, RoutedEventArgs e)
+        {
+            hashtable.Remove(Key_TxtBox.Text.ToString());
+        }
+        private void ShowAllBtn_Click(object sender, RoutedEventArgs e)
+        {
+            ICollection collection = hashtable.Keys;
+            string allValue = "All Value :\n";
+            foreach (string key in collection) {
+                allValue = allValue+"  "+hashtable[key]+"\n";
+            }
+            MessageBox.Show(allValue);
+        }
+
+
+
+
+
+
+        private void Key_gotFocus(object sender, RoutedEventArgs e)
+        {
+            if (Key_TxtBox.Text=="Key") {
+                Key_TxtBox.Text = ""; 
+            }
+            Key_TxtBox.Foreground = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FF434343"));
+        }
+
+        private void Key_lostFocus(object sender, RoutedEventArgs e)
+        {
+            if (Key_TxtBox.Text == "") {
+                Key_TxtBox.Text = "Key";
+                Key_TxtBox.Foreground = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FFD3D3D3"));
+
+            }
+        }
+
+
+        private void Value_gotFocus(object sender, RoutedEventArgs e)
+        {
+            if (Value_TxtBox.Text == "Value")
+            {
+                Value_TxtBox.Text = "";
+            }
+            Value_TxtBox.Foreground = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FF434343"));
+        }
+
+        private void Value_lostFocus(object sender, RoutedEventArgs e)
+        {
+            if (Value_TxtBox.Text == "")
+            {
+                Value_TxtBox.Text = "Value";
+                Value_TxtBox.Foreground = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FFD3D3D3"));
+
+            }
+        }
+
+
     }
 }
